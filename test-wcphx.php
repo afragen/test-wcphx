0d507be66e7360e59ddf2cd57d1a3b049f3a677e
Andy Fragen <andy@thefragens.com>
2016-09-17 18:18:58 -0700

    initial plugin setup

---
+++ test-wcphx.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name:       Test WCPHX
+ * Plugin URI:        https://github.com/afragen/test-wcphx
+ * Version:           0.1
+ * Author:            Andy Fragen
+ * Description:       This is a test plugin for WordCamp Phoenix 2016 demo. It is empty.
+ * GitHub Plugin URI: afragen/test-wcphx
+ * GitHub Branch:     master
+ */
